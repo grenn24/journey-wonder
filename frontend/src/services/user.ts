430d0ceee81cc06c36ba8b56bc5eb4bccd0b495b
Hoo Di Heng <Hoodiheng@gmail.com>
2025-02-12 08:00:55 +0800

    minor changes

--- frontend/src/services/user.ts
+++ frontend/src/services/user.ts
@@ -1,11 +1,12 @@
 import ApiClient from "../utilities/apiClient";
 import config from "config";
+import User from "../../../backend/models/user"
 
 class UserService {
 	apiClient = new ApiClient(import.meta.env.VITE_BACKEND_BASEURL + "/user");
 	getUserByID(userID: string) {
 		const controller = new AbortController();
-		const response = this.apiClient.get(`/${userID}`, {
+		const response = this.apiClient.get<typeof User>(`/${userID}`, {
 			signal: controller.signal,
 		});
 		return { response, abort: () => controller.abort() };
